fac781323ef3bb8ee40fb4f35be2964869035a83
zivhd <zivprodriguez@gmail.com>
2024-08-17 05:46:07 +0800

    feat: added camera permissions

--- app/src/main/java/com/example/lubak/screen/CameraScreen.kt
+++ app/src/main/java/com/example/lubak/screen/CameraScreen.kt
@@ -1,12 +1,84 @@
 package com.example.lubak.screen
 
-import androidx.compose.foundation.layout.Box
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 
 @Composable
 fun CameraScreen() {
-
-    Box(){
-
+    var hasCameraPermission by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        hasCameraPermission = isGranted
     }
+    LaunchedEffect(key1 = Unit) {
+        hasCameraPermission = ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+    if (hasCameraPermission) {
+        CameraComponent()
+    } else {
+        CameraPermissionNeeded(launcher)
+    }
+
+}
+
+@Composable
+fun CameraPermissionNeeded(launcher: ManagedActivityResultLauncher<String, Boolean>) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+        Text("Camera Permission Needed", fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text("The application needs access to your camera, which is required to contribute and share the potholes you see!")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { launcher.launch(Manifest.permission.CAMERA) }) {
+            Text(text = "Request Camera Permission")
+        }
+    }
+
+}
+
+@Composable
+fun CameraComponent(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+    Text("Camera Permission Given", fontWeight = FontWeight.Bold)}
 }
