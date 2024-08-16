1baf7ff9e043dcac6f64f602e9ffe2219b4bc359
zivhd <zivprodriguez@gmail.com>
2024-08-16 13:52:43 +0800

    feat: add navigation drawer and top bar to main screen

--- app/src/main/java/com/example/lubak/MainActivity.kt
+++ app/src/main/java/com/example/lubak/MainActivity.kt
@@ -4,39 +4,125 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lubak.ui.theme.LubakTheme
 import com.mapbox.geojson.Point
 import com.mapbox.maps.MapboxExperimental
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.MapViewportState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
-    @OptIn(MapboxExperimental::class)
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MapboxMap(
-                Modifier.fillMaxSize(),
-                mapViewportState = MapViewportState().apply {
-                    setCameraOptions {
-                        zoom(2.0)
-                        center(Point.fromLngLat(-98.0, 39.5))
-                        pitch(0.0)
-                        bearing(0.0)
+            LubakTheme {
+                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                val scope = rememberCoroutineScope()
+                ModalNavigationDrawer(
+                    drawerState = drawerState,
+                    drawerContent = {
+                    ModalDrawerSheet {
+                        Text("Lubak", modifier = Modifier.padding(16.dp))
+                        HorizontalDivider()
+                        NavigationDrawerItem(
+                            label = { Text(text = "Profile")},
+                            selected = false,
+                            onClick = {})
                     }
                 },
-            )
 
+                ) {
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        MyTopAppBar(scope,drawerState)
+                    },
+                ) { innerPadding ->
+                    LubakMap(modifier = Modifier.padding(innerPadding))
+                }
+            }
         }
     }
 }
+
+@OptIn(MapboxExperimental::class)
+@Composable
+fun LubakMap(modifier: Modifier = Modifier) {
+    MapboxMap(
+        modifier = modifier,
+        mapViewportState = MapViewportState().apply {
+            setCameraOptions {
+                zoom(1.0)
+                center(Point.fromLngLat(-98.0, 39.5))
+                pitch(0.0)
+                bearing(0.0)
+            }
+        },
+    )
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopAppBar(scope: CoroutineScope,drawerState: DrawerState ) {
+
+        TopAppBar(colors = topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ), title = {
+            Text("Lubak")
+        }, actions = {
+            IconButton(onClick = { scope.launch {
+                drawerState.apply {
+                    if(isClosed) open() else close()
+                }
+            } }) {
+                Icon(Icons.Filled.MoreVert, contentDescription = "More options")
+            }
+        })
+    }
+
+
+}
+
+
+
 
