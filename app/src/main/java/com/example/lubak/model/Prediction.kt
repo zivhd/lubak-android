93b16d5b964ab3f677b3cd178266c765e5dfb7d7
zivhd <zivprodriguez@gmail.com>
2024-08-18 12:12:22 +0800

    fix: fixed microservice connection

--- app/src/main/java/com/example/lubak/model/Prediction.kt
+++ app/src/main/java/com/example/lubak/model/Prediction.kt
@@ -1,11 +1,19 @@
 package com.example.lubak.model
 
 data class PredictionResponse(
+    val predictions: PredictionsDetails
+)
+
+data class PredictionsDetails(
     val predictions: List<Prediction>
 )
 
 data class Prediction(
-    val `class`: Int,
-    val confidence: Float,
-    val box: List<Float>
+    val objects: List<ObjectDetail>
+)
+
+data class ObjectDetail(
+    val bottom_center: List<Int>,
+    val bounding_box: List<Int>,
+    val `class`: Int
 )
--- app/src/main/java/com/example/lubak/view/PredictScreen.kt
+++ app/src/main/java/com/example/lubak/view/PredictScreen.kt
@@ -4,7 +4,9 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
@@ -14,6 +16,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
@@ -37,29 +40,36 @@ fun PredictScreen(cameraViewModel: CameraViewModel) {
     var errorMessage by remember { mutableStateOf<String?>(null) }
 
     if (byteArray != null) {
-        Log.d("testtt","testttttttt")
         // Convert byteArray to MultipartBody.Part
         val requestBody = byteArray.toRequestBody("image/jpeg".toMediaTypeOrNull())
         val imagePart = MultipartBody.Part.createFormData("image", "image.jpg", requestBody)
 
         LaunchedEffect(byteArray) {
             try {
-                val response = apiService.predict(imagePart).execute()
+                val response = apiService.predict(imagePart)
                 predictionResponse = response.body()
             } catch (e: Exception) {
                 errorMessage = e.localizedMessage
             }
         }
 
+
+    } else {
+        Text("No image to display")
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+        , verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         if (predictionResponse != null) {
-            Text("Predictions: ${predictionResponse!!.predictions}")
+            Text("Pothole Detected")
         } else if (errorMessage != null) {
             Text("Error: $errorMessage")
+            Log.e("Error","$errorMessage")
         } else {
             Text("Sending image for prediction...")
         }
-    } else {
-        Text("No image to display")
     }
 }
 
